2b746ac8ca7667ac472e06cc16ff4f96d0151c37
hajime-8123 <117512935+hajime-8123@users.noreply.github.com>
2023-10-13 21:32:39 +0900

    Update script.sh

--- week6/Using Fast DDS/script.sh
+++ week6/Using Fast DDS/script.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+# First setup discovery server
+fastdds discovery --server-id 0 &
+
+# Wait for the discovery server to start
+sleep 2
+
+# Set the environment variable for ROS discovery server
+export ROS_DISCOVERY_SERVER=127.0.0.1:11811
+
+# Launch listener node
+ros2 run demo_nodes_cpp listener --ros-args --remap __node:=listener_discovery_server &
+
+# Launch talker node in a new terminal
+gnome-terminal -- bash -c "export ROS_DISCOVERY_SERVER=127.0.0.1:11811; ros2 run demo_nodes_cpp talker --ros-args --remap __node:=talker_discovery_server" &
+
+# Finally, check that everything is running correctly
+ros2 run demo_nodes_cpp talker --ros-args --remap __node:=simple_talker
 
